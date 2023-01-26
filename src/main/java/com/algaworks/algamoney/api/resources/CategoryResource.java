309803480043c36cbfd1c08fd3142fb22016a70d
rondinelli-oliveira <rondinelliads@gmail.com>
2023-01-26 20:27:48 -0300

    category save and https status created(201)

--- src/main/java/com/algaworks/algamoney/api/resources/CategoryResource.java
+++ src/main/java/com/algaworks/algamoney/api/resources/CategoryResource.java
@@ -2,11 +2,13 @@ package com.algaworks.algamoney.api.resources;
 
 import com.algaworks.algamoney.api.models.Category;
 import com.algaworks.algamoney.api.repositories.CategoryRepository;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -19,5 +21,16 @@ public class CategoryResource {
     @GetMapping("/categories")
     public List<Category> findAll() {
         return categoryRepository.findAll();
+    }
+
+    @PostMapping("/categories")
+    public ResponseEntity<Category> save(@RequestBody Category category, HttpServletResponse response) {
+        Category categorySave = categoryRepository.save(category);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(categorySave.getId()).toUri();
+        response.setHeader("Location", uri.toASCIIString());
+
+        return ResponseEntity.created(uri).body(categorySave);
     }
 }
