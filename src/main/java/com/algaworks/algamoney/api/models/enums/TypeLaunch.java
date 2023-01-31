060d9313ccbd33b81d7074b3112ae593e8a35848
rondinelli-oliveira <rondinelliads@gmail.com>
2023-01-30 22:08:55 -0300

    create mingration and entity launch

---
+++ src/main/java/com/algaworks/algamoney/api/models/enums/TypeLaunch.java
@@ -0,0 +1,6 @@
+package com.algaworks.algamoney.api.models.enums;
+
+public enum TypeLaunch {
+    RECEIPT,
+    EXPENSE
+}
---
+++ src/main/java/com/algaworks/algamoney/api/models/launch.java
@@ -0,0 +1,119 @@
+package com.algaworks.algamoney.api.models;
+
+import com.algaworks.algamoney.api.models.enums.TypeLaunch;
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+@Entity
+@Table
+public class launch {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String description;
+
+    @Column(name = "due_date")
+    private LocalDate dueDate;
+    private LocalDate payday;
+    private Double value;
+    private String note;
+
+    @Enumerated(EnumType.STRING)
+    private TypeLaunch type;
+
+    @ManyToOne
+    @JoinColumn(name = "id_category")
+    private Category category;
+
+    @ManyToOne
+    @JoinColumn(name = "id_person")
+    private Person person;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(LocalDate dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public LocalDate getPayday() {
+        return payday;
+    }
+
+    public void setPayday(LocalDate payday) {
+        this.payday = payday;
+    }
+
+    public Double getValue() {
+        return value;
+    }
+
+    public void setValue(Double value) {
+        this.value = value;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    public TypeLaunch getType() {
+        return type;
+    }
+
+    public void setType(TypeLaunch type) {
+        this.type = type;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public void setPerson(Person person) {
+        this.person = person;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        launch launch = (launch) o;
+        return Objects.equals(id, launch.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
