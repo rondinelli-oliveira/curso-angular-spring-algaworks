745d26a95153c4e3e13bd0ace4837400096aceaf
rondinelli-oliveira <rondinelliads@gmail.com>
2023-01-30 23:09:18 -0300

    save launch

--- src/main/java/com/algaworks/algamoney/api/resources/LaunchResource.java
+++ src/main/java/com/algaworks/algamoney/api/resources/LaunchResource.java
@@ -1,13 +1,15 @@
 package com.algaworks.algamoney.api.resources;
 
+import com.algaworks.algamoney.api.event.ResouceCreatedEvent;
 import com.algaworks.algamoney.api.models.Launch;
 import com.algaworks.algamoney.api.repositories.LaunchRepository;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -19,6 +21,9 @@ public class LaunchResource {
     @Autowired
     LaunchRepository launchRepository;
 
+    @Autowired
+    private ApplicationEventPublisher publisher;
+
     @GetMapping("/launchies")
 
     public List<Launch> findAll() {
@@ -29,5 +34,12 @@ public class LaunchResource {
     public ResponseEntity<Launch> findById(@PathVariable Long id) {
         Optional<Launch> launch = launchRepository.findById(id);
         return launch.isPresent() ? ResponseEntity.ok(launch.get()) : ResponseEntity.notFound().build();
+    }
+
+    @PostMapping("/launchies")
+    public ResponseEntity<Launch> save(@Valid @RequestBody Launch launch, HttpServletResponse response) {
+        Launch launchSave = launchRepository.save(launch);
+        publisher.publishEvent(new ResouceCreatedEvent(this, response, launchSave.getId()));
+        return ResponseEntity.status(HttpStatus.CREATED).body(launchSave);
     }
 }
